c9a131cabfc9be695d5abf5f44dbc22fbdaa8200
Emanuel Costa <emanuelcosta28@outlook.com>
2024-06-10 20:30:29 +0100

    Adds work section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import Image from "next/image";
+import Section from "@/app/components/Section";
+import Workplaces from "@/app/components/Workplaces";
+
+import coremediaLogo from "@/public/jobs/coremedia.jpeg";
+import optigestLogo from "@/public/jobs/optigest.jpeg";
+import nemobileLogo from "@/public/jobs/nemobile.jpeg";
 
 export default function Home() {
   return (
@@ -10,6 +15,42 @@ export default function Home() {
           </h1>
         </div>
       </div>
+      <Section heading="Work" headingAlignment="left">
+        <div className="flex w-full flex-col gap-8">
+          <p>
+            Experienced Full Stack Web Developer with a passion for creating
+            dynamic and innovative solutions. With a passion for crafting
+            dynamic web applications, I bring a wealth of expertise in
+            integrating cutting-edge technologies and frameworks to create
+            seamless user experiences.
+          </p>
+          <Workplaces items={workplaces} />
+        </div>
+      </Section>
     </div>
   );
 }
+
+const workplaces = [
+  {
+    title: "Full Stack Engineer",
+    company: "Coremedia",
+    date: "2021 - Present",
+    imageSrc: coremediaLogo,
+    link: "https://coremedia.com/",
+  },
+  {
+    title: "Full Stack Web Developer",
+    company: "Optigest",
+    date: "2020 - 2021",
+    imageSrc: optigestLogo,
+    link: "https://www.optigest.net/www/PT/",
+  },
+  {
+    title: "Full Stack Web Developer",
+    company: "Nemobile",
+    date: "2020",
+    imageSrc: nemobileLogo,
+    link: "https://sidis.ai/",
+  },
+];
